db516449cc4273221ea67d1174ae419a45cb08cd
HungTran <hungdng.92@gmail.com>
2020-10-27 11:29:08 +0700

    lol

--- src/main/java/hung/tran/junit/web/WidgetRestController.java
+++ src/main/java/hung/tran/junit/web/WidgetRestController.java
@@ -5,6 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.google.common.net.HttpHeaders;
+import com.sun.tools.javac.main.Option;
+
 import hung.tran.junit.model.Widget;
 import hung.tran.junit.service.WidgetService;
 
@@ -13,6 +16,7 @@ import org.apache.logging.log4j.Logger;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 
@@ -66,5 +70,53 @@ public class WidgetRestController {
 		} catch (URISyntaxException e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
+	}
+	
+	@PutMapping("/rest/widget/{id}")
+	public ResponseEntity<Widget> updateWidget(@RequestBody Widget widget, @PathVariable Long id, @RequestHeader(HttpHeaders.IF_MATCH) String ifMatch){
+		// Get the widget with the specified id
+		Optional<Widget> existingWidget = widgetService.findById(id);
+		if (!existingWidget.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		// Validate that the if-match header matches the widget's version
+		if (!ifMatch.equalsIgnoreCase(Integer.toString(existingWidget.get().getVersion()))) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
+		// Update the widget
+		widget.setId(id);
+		widget = widgetService.save(widget);
+		
+		try {
+			// Return a 200 response with the updated widget
+			return ResponseEntity
+					.ok()
+					.eTag(Integer.toString(widget.getVersion()))
+					.location(new URI("/rest/widget/" + widget.getId()))
+					.body(widget);
+		} catch (URISyntaxException e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+	
+	@PutMapping("/rest/proper/widget/{id}")
+	public ResponseEntity<Widget> updateWidgetProper(@RequestBody Widget widget, @PathVariable Long id, @RequestHeader(HttpHeaders.IF_MATCH) Integer ifMatch){
+		Optional<Widget> existingWidget = widgetService.findById(id);
+		if (existingWidget.isPresent()) {
+			if (ifMatch.equals(existingWidget.get().getVersion())) {
+				widget.setId(id);
+				return ResponseEntity.ok().body(widgetService.save(widget));
+			} else {
+				return ResponseEntity.status(HttpStatus.CONFLICT).build();
+			}
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@DeleteMapping("/rest/widget/{id}")
+	public ResponseEntity<?> deleteWidget(@PathVariable Long id) {
+		widgetService.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 }
